09c21161fe6bfbbffe26d2cd994908a6f6d6b261
evan <evanmc@yahoo.com>
2022-08-07 15:39:26 -0700

    Add option for grab only -- no processing, just taking pictures

--- jetson/weeds.py
+++ jetson/weeds.py
@@ -31,7 +31,7 @@ from Performance import Performance
 from Reporting import Reporting
 from Treatment import Treatment
 from MUCCommunicator import MUCCommunicator
-from Messages import OdometryMessage
+from Messages import OdometryMessage, SystemMessage, TreatmentMessage
 
 #from Selection import Selection
 
@@ -68,7 +68,7 @@ group.add_argument("-f", "--forest", action="store_true", default=False, help="P
 group.add_argument("-g", "--gradient", action="store_true", default=False, help="Predict using gradient boosting. Requires data file to be specified")
 group.add_argument("-svm", "--support", action="store_true", default=False, help="Predict using support vector machine. Requires data file to be specified")
 parser.add_argument("-im", "--image", action="store", default=200, type=int, help="Horizontal length of image")
-parser.add_argument("-lg", "--logging", action="store", default="info-logging.yaml", help="Logging configuration file")
+parser.add_argument("-lg", "--logging", action="store", default="logging.ini", help="Logging configuration file")
 parser.add_argument("-m", "--mask", action="store_true", default=False, help="Mask only -- no processing")
 parser.add_argument("-ma", "--minarea", action="store", default=500, type=int, help="Minimum area of a blob")
 parser.add_argument("-mr", "--minratio", action="store", default=5, type=int, help="Minimum size ratio for classifier")
@@ -173,7 +173,7 @@ def startupPerformance() -> Performance:
 #     log.debug("Callback for distance")
 #     return
 
-def startupCommunications(options: OptionsFile, callbackOdometer: Callable, callbackSystem: Callable) -> ():
+def startupCommunications(options: OptionsFile, callbackOdometer: Callable, callbackSystem: Callable, callbackTreatment: Callable) -> ():
     """
 
     :param options:
@@ -187,27 +187,38 @@ def startupCommunications(options: OptionsFile, callbackOdometer: Callable, call
     #     options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_ROOM_ODOMETRY)))
 
     # The room that will get the announcements about forward or backward progress
-    odometryRoom = MUCCommunicator(options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_JID_JETSON),
+    odometryRoom = MUCCommunicator(options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_SERVER),
+                                   options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_JID_JETSON),
                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_NICK_JETSON),
                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_DEFAULT_PASSWORD),
                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_ROOM_ODOMETRY),
-                                   callbackOdometer)
+                                   callbackOdometer,
+                                   None)
 
     # The room that will get status reports about this process
-    systemRoom = MUCCommunicator(options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_JID_JETSON),
+    systemRoom = MUCCommunicator(options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_SERVER),
+                                 options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_JID_JETSON),
                                  options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_NICK_JETSON),
                                  options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_DEFAULT_PASSWORD),
                                  options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_ROOM_SYSTEM),
-                                 callbackSystem)
+                                 callbackSystem,
+                                 None)
 
+    treatmentRoom = MUCCommunicator(options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_SERVER),
+                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_JID_JETSON),
+                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_NICK_JETSON),
+                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_DEFAULT_PASSWORD),
+                                    options.option(constants.PROPERTY_SECTION_XMPP, constants.PROPERTY_ROOM_TREATMENT),
+                                    callbackTreatment,
+                                    None)
     #print("XMPP communications started")
 
-    return (odometryRoom, systemRoom)
+    return (odometryRoom, systemRoom, treatmentRoom)
 #
 # L O G G E R
 #
 
-def startupLogger(outputDirectory: str) -> Logger:
+def startupLogger(outputDirectory: str) -> ():
     """
     Initializes two logging systems: the image logger and python centric logging.
     :param outputDirectory: The output directory for the images
@@ -359,13 +370,25 @@ if arguments.contours:
     featuresToShow.append(constants.NAME_CONTOUR)
 
 imageNumber = 0
+processing = False
 
 def storeImage() -> bool:
+    global imageNumber
+
+    if not processing:
+        log.info("Not collecting images (This is normal if the weeding has not started")
+        return False
+
     if arguments.verbose:
         print("Processing image " + str(imageNumber))
     log.info("Processing image " + str(imageNumber))
     performance.start()
-    rawImage = camera.capture()
+    try:
+        rawImage = camera.capture()
+    except IOError as e:
+        log.fatal("Cannot capture image. ({})".format(e))
+        return False
+
     performance.stopAndRecord(constants.PERF_ACQUIRE)
 
     # ImageManipulation.show("Source",image)
@@ -373,6 +396,17 @@ def storeImage() -> bool:
 
     manipulated = ImageManipulation(rawImage, imageNumber, logger)
     logger.logImage("original", manipulated.image)
+
+    # Send out a message to the treatment channel that an image has been taken
+    message = TreatmentMessage()
+    message.plan = constants.Treatment.RAW_IMAGE
+    message.name = "original"
+    messageText = message.formMessage()
+    log.debug("Sending: {}".format(messageText))
+    roomTreatment.sendMessage(messageText)
+
+    imageNumber += 1
+    return True
 
 def processImage() -> bool:
     global imageNumber
@@ -638,12 +672,26 @@ keepAliveMessages = 0
 # When the total distance is the width of the image, grab an image and process it.
 #
 def messageSystemCB(conn,msg: xmpp.protocol.Message):
+    global logger
+    global processing
     # Make sure this is a message sent to the room, not directly to us
     if msg.getType() == "groupchat":
             body = msg.getBody()
             # Check if this is a real message and not just an empty keep-alive message
             if body is not None:
                 log.debug("system message from {}: [{}]".format(msg.getFrom(), msg.getBody()))
+                systemMessage = SystemMessage(raw=msg.getBody())
+                if systemMessage.action == constants.Action.START.name:
+                    processing = True
+                    sessionName = systemMessage.name
+                    outputDirectory = arguments.output + "/" + sessionName
+                    log.debug("Begin processing to: {}".format(outputDirectory))
+                    logger = Logger()
+                    if not logger.connect(outputDirectory):
+                        log.error("Unable to connect to logging. {} does not exist.".format(outputDirectory))
+                if systemMessage == constants.Action.STOP:
+                    processing = False
+
     elif msg.getType() == "chat":
             print("private: " + str(msg.getFrom()) +  ":" +str(msg.getBody()))
     else:
@@ -665,11 +713,21 @@ def messageOdometryCB(conn, msg: xmpp.protocol.Message):
                 log.debug("Distance message from {}: [{}]".format(msg.getFrom(), msg.getBody()))
                 odometryMessage = OdometryMessage(raw=body)
                 totalMovement += odometryMessage.distance
-                log.debug("Total movement is {}".format(totalMovement))
+                # The time of the observation
+                timeRead = odometryMessage.timestamp
+                # Determine how old the observation is
+                # The version of python on the jetson does not support time_ns, so this a bit of a workaround until I
+                # get that sorted out.  Just convert the reading to milliseconds for now
+                #timeDelta = (time.time() * 1000) - (timeRead / 1000000)
+                timeDelta = (time.time() * 1000) - timeRead
+                log.debug("Total movement is {} at time {}. Time now is {} delta from now {} ms".format(totalMovement, timeRead, time.time() * 1000, timeDelta))
 
-                # If the movement is equal to the image size, process the image
-                # Clearly, this is wrong -- just a placeholder for now.
-                if totalMovement % 100 == 0:
+                if timeDelta > 5000:
+                    log.debug("Old message seen.  Ignored")
+
+                    # If the movement is equal to the image size, process the image
+                    # Clearly, this is wrong -- just a placeholder for now.
+                elif totalMovement % 100 == 0:
                     processor()
             else:
                 # There's not much to do here for keepalive messages
@@ -680,6 +738,22 @@ def messageOdometryCB(conn, msg: xmpp.protocol.Message):
     else:
         log.error("Unknown message type {}".format(msg.getType()))
 
+# The callback for messages received in the system room.
+# When the total distance is the width of the image, grab an image and process it.
+#
+def messageTreatmentCB(conn,msg: xmpp.protocol.Message):
+    # Make sure this is a message sent to the room, not directly to us
+    if msg.getType() == "groupchat":
+            body = msg.getBody()
+            # Check if this is a real message and not just an empty keep-alive message
+            if body is not None:
+                log.debug("treatment message from {}: [{}]".format(msg.getFrom(), msg.getBody()))
+    elif msg.getType() == "chat":
+            print("private: " + str(msg.getFrom()) +  ":" +str(msg.getBody()))
+    else:
+        log.error("Unknown message type {}".format(msg.getType()))
+
+#
 #
 # This method will never return.  Connect and start processing messages
 #
@@ -704,7 +778,8 @@ def takeImages(camera: CameraBasler):
     if camera.connect():
         try:
             camera.initialize()
-            camera.start()
+            #camera.start()
+            camera.startGrabbingImages()
         except IOError as io:
             camera.log.error(io)
         rc = 0
@@ -721,7 +796,7 @@ options = readINI()
 camera = startupCamera(options)
 log.debug("Camera started")
 
-(roomOdometry, roomSystem) = startupCommunications(options, messageOdometryCB, messageSystemCB)
+(roomOdometry, roomSystem, roomTreatment) = startupCommunications(options, messageOdometryCB, messageSystemCB, messageTreatmentCB)
 log.debug("Communications started")
 #odometer = startupOdometer(processImage)
 
@@ -733,13 +808,21 @@ mmPerPixel = camera.getMMPerPixel()
 threads = list()
 log.debug("Starting odometry receiver")
 generator = threading.Thread(name=constants.THREAD_NAME_ODOMETRY, target=processMessages, args=(roomOdometry,))
+generator.daemon = True
 threads.append(generator)
 generator.start()
 
 log.debug("Starting system receiver")
 sys = threading.Thread(name=constants.THREAD_NAME_SYSTEM, target=processMessages, args=(roomSystem,))
+sys.daemon = True
 threads.append(sys)
 sys.start()
+
+log.debug("Starting treatment thread")
+treat = threading.Thread(name=constants.THREAD_NAME_TREATMENT, target=processMessages, args=(roomTreatment,))
+treat.daemon = True
+threads.append(treat)
+treat.start()
 
 # Start the thread that will begin acquiring images
 log.debug("Start image acquisition")
