26998dba74641fd59c39b5676223418e3ac4055f
Evan McGinnis <evanmc@yahoo.com>
2022-11-02 20:59:16 +0100

    Add new state for missing

--- lib/Camera.py
+++ lib/Camera.py
@@ -11,17 +11,27 @@ class CameraState(StateMachine):
     capturing = State('Capturing')
     claimed = State('Claimed')
     idle = State('Idle')
+    failed = State('Failed')
+    missing = State('Missing')
 
     toIdle = new.to(idle)
     toCapture = claimed.to(capturing)
     toStop = capturing.to(idle)
     toClaim = idle.to(claimed)
+    toFailed = claimed.to(failed)
+    toMissing = new.to(missing)
+
 
 class Camera(ABC):
     def __init__(self, **kwargs):
         super().__init__()
-        self.state = CameraState()
+        self._state = CameraState()
 
+
+    @property
+    def state(self) -> CameraState:
+        return self._state
+        return
 
     @abstractmethod
     def connect(self):
