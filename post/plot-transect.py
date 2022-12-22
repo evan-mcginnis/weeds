bd8b59c2b5e69ae4f7b9a18f52148fb7a4f30dd5
Evan McGinnis <evanmc@yahoo.com>
2022-12-22 00:24:51 +0000

    Add error checking

--- post/plot-transect.py
+++ post/plot-transect.py
@@ -2,29 +2,37 @@
 # Visualize the transect of the laser level
 #
 #
+# This will plot the values of the red band of an image, identifying the maximum value found there.
+# The image tested is black with the exception of the red laser beam shown horizontally across the image.
 
 import argparse
-
-import PIL
 import matplotlib.pyplot as plt
-import matplotlib.image
 import numpy as np
 import cv2 as cv
-from PIL import Image
+import sys
+import os
 
 parser = argparse.ArgumentParser("Visualize transect")
 parser.add_argument("-i", "--input", action="store", required=True, help="Image to process")
-parser.add_argument("-t", "--threshold", action="store", required=False, default=50, help="Threshold of spike")
 parser.add_argument("-p", "--position", action="store", required=False, default=1000, type=int, help="Position along x axis")
 arguments = parser.parse_args()
 
 image = None
 RED = 2
 
+if not os.path.exists(arguments.input):
+    print("Unable to find file: {}".format(arguments.input))
+    sys.exit(-1)
+
 try:
     image = cv.imread(arguments.input, cv.IMREAD_COLOR)
 except FileNotFoundError:
     print("Unable to find the file: {}".format(arguments.input))
+    sys.exit(-1)
+
+if image is None:
+    print("Unable to read image: {}".format(arguments.input))
+    sys.exit(-1)
 
 # Extract the transect
 transect = image[:, arguments.position, RED]
@@ -37,7 +45,5 @@ plt.text(30, 62, "Max value = {} at ({},{})".format(np.max(transect), arguments.
 plt.grid(True)
 plt.show()
 
-# if image is not None:
-#     plt.imshow(image, interpolation='none', vmin=430, vmax=470)
-#     plt.show()
+
 
