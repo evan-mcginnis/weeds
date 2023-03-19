e7255a4bf092a6bcb5045d335af3dabfb1e2d1ca
Evan McGinnis <evanmc@yahoo.com>
2023-03-19 15:07:28 +0100

    Minor changes to move things to the GPSPoller class

--- rio/GPSClient.py
+++ rio/GPSClient.py
@@ -74,35 +74,6 @@ class GPS:
         return position
 
 # Example for another library
-#from gps import *
-# class GpsPoller(threading.Thread):
-#
-#    def __init__(self):
-#        threading.Thread.__init__(self)
-#        self.session = gps(mode=WATCH_ENABLE)
-#        self.current_value = None
-#
-#    def get_current_value(self):
-#        return self.current_value
-#
-#    def run(self):
-#        try:
-#             while True:
-#                 self.current_value = self.session.next()
-#                 time.sleep(0.2) # tune this, you might not get values that quickly
-#        except StopIteration:
-#             pass
-
-if __name__ == '__main__':
-
-    # # This is one way of doing things
-    # gpsp = GpsPoller()
-    # gpsp.start()
-    # # gpsp now polls every .2 seconds for new data, storing it in self.current_value
-    # while 1:
-    #     # In the main thread, every 5 seconds print the current value
-    #     time.sleep(5)
-    #     print(gpsp.get_current_value())
 
     # This is another
 
@@ -120,7 +91,7 @@ if __name__ == '__main__':
             print("Position: {}".format(packet.position()))
             print("Error: {}".format(packet.position_precision()))
             print("Fix: {}".format(packet.mode))
-            print("Elapsed time {} ms".format(finish - start))
+            print("Elapsed time {} seconds".format(finish - start))
         except gpsd.NoFixError as fix:
             print("Unable to obtain a 2D fix")
     else:
