05bbe8334a61b0c751ea60e9d7808b5476abd57e
Evan McGinnis <evanmc@yahoo.com>
2024-06-04 18:19:25 +0100

    checkpoint

--- post/plot-depth.py
+++ post/plot-depth.py
@@ -5,6 +5,7 @@
 # The data must be in numpy format
 #
 
+import sys
 import argparse
 
 import PIL
@@ -16,6 +17,13 @@ from PIL import Image
 parser = argparse.ArgumentParser("Visualize Depth Data")
 parser.add_argument("-i", "--input", action="store", required=True, help="Depth data file in npy format")
 parser.add_argument("-o", "--output", action="store", required=False, help="Image to be saved")
+parser.add_argument("-n", "--vmin", action="store", required=False, type=int, help="vmin for plot")
+parser.add_argument("-x", "--vmax", action="store", required=False, type=int, help="vmax for plot")
+parser.add_argument("-t", "--threshold", action="store", required=False, type=int, help="Ground threshold")
+parser.add_argument("-c", "--correct", action="store_true", required=False, default=False, help="Correct depth")
+parser.add_argument("-g", "--grayscale", action="store_true", required=False, default=False, help="Covert to greyscale")
+parser.add_argument("-no", "--normalize", action="store_true", required=False, default=False, help="Normalize between 0 and 1")
+parser.add_argument("-hg", "--histogram", action="store_true", required=False, default=False, help="Plot histogram")
 arguments = parser.parse_args()
 
 depth = None
@@ -24,16 +32,73 @@ try:
     depth = np.load(arguments.input)
 except FileNotFoundError:
     print("Unable to find the file: {}".format(arguments.input))
+    sys.exit(-1)
 
+# Clean up the invalid values by setting them to zero
+depth[depth == 65535] = 0
+
+if arguments.threshold is not None:
+    depth[depth > arguments.threshold] = 0
+
+
+
+if arguments.vmin is None:
+    minval = np.min(depth[np.nonzero(depth)])
+else:
+    minval = arguments.vmin
+
+if arguments.vmax is None:
+    maxval = np.max(depth[np.nonzero(depth)])
+else:
+    maxval = arguments.vmax
+
+print(f"There are {len(depth[depth == 0])} readings for 0")
+depth[depth == 0] = maxval
+
+if arguments.correct:
+    height, width = depth.shape
+    standardeviation = np.std(depth, where=depth > 0)
+
+    for h in range(height - 1):
+        zeroSeen = False
+        for w in range(width - 2):
+            if abs(depth[h, w] - depth[h, w + 1]) < standardeviation:
+                depth[h, w] = 0
+
+# bins = np.linspace(np.min(depth[np.nonzero(depth)]), np.max(depth), 1000, dtype=float)
+# print(bins)
+# hist, bins = np.histogram(depth, bins=bins)
+# print(hist)
 
 if depth is not None:
-    print("Min/Max: {}/{}".format(depth.min(), depth.max()))
+    print("Min/Max: {}/{}".format(minval, maxval))
+    print(f"Min {minval} Max {maxval} Avg {np.average(depth)} Std Deviation {np.std(depth, where=depth>0)}")
     print("Shape is: {}".format(np.shape(depth)))
-    if arguments.output is not None:
-        plt.imsave(arguments.output, depth, vmin=250, vmax=340)
-    else:
-        plt.imshow(depth, interpolation='none', vmin=250, vmax=340)
+
+    if arguments.normalize:
+        normalizedData = (depth - np.min(depth)) / (np.max(depth) - np.min(depth))
+    if arguments.histogram:
+        bins = np.linspace(0, 1, 1000, dtype=float)
+        hist, bins = np.histogram(normalizedData, bins=bins)
+        plt.plot(hist)
         plt.show()
+    if arguments.grayscale:
+        # Normalize the depth data between 0 and 1
+        normalizedData = (depth - np.min(depth)) / (np.max(depth) - np.min(depth))
+        plt.imshow(normalizedData, cmap='gray', vmin=0, vmax=1)
+        plt.show()
+        # Normalize the depth data between 0 and 255
+        # normalizedData = np.zeros_like(depth, dtype="float64")
+        # np.copyto(normalizedData, depth)
+        # normalizedData *= (255.0 / depth.max())
+        # plt.imshow(normalizedData, cmap='gray', vmin=0, vmax=255)
+        # plt.show()
+    else:
+        if arguments.output is not None:
+            plt.imsave(arguments.output, depth, vmin=250, vmax=340)
+        else:
+            plt.imshow(depth, interpolation='none', vmin=minval, vmax=maxval)
+            plt.show()
 
 # if depth is not None:
 #     im = Image.fromarray(depth)
--- post/plot-transect.py
+++ post/plot-transect.py
@@ -5,6 +5,13 @@
 # This will plot the values of the red band of an image, identifying the maximum value found there.
 # The image tested is black with the exception of the red laser beam shown horizontally across the image.
 
+#
+# D E P R E C A T E D
+#
+# Use plot.py in the util directory instead
+#
+
+
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,7 +21,9 @@ import os
 
 parser = argparse.ArgumentParser("Visualize transect")
 parser.add_argument("-i", "--input", action="store", required=True, help="Image to process")
-parser.add_argument("-p", "--position", action="store", required=False, default=1000, type=int, help="Position along x axis")
+parser.add_argument("-x", "--x", action="store", required=False, default=0, type=int, help="Position along x axis")
+parser.add_argument("-y", "--y", action="store", required=True, type=int, help="Position along y axis")
+parser.add_argument("-l", "--length", action="store", required=False, default=-1, type=int, help="Length of transect")
 parser.add_argument("-b", "--band", action="store", required=False, default="red", choices=["red", "blue", "green"], type=str, help="BGR band")
 arguments = parser.parse_args()
 
@@ -45,13 +54,26 @@ elif arguments.band == "blue":
 elif arguments.band == "green":
     band = GREEN
 
-transect = image[:, arguments.position, band]
+height, width, _ = image.shape
+
+if arguments.length == -1:
+    transect = image[:, arguments.y, band]
+else:
+    # The transect is from the x specified to the width
+    if arguments.x not in range(width):
+        print(f"Specified x outside width of image {width}")
+        exit(-1)
+    if arguments.x + arguments.length not in range(width):
+        print(f"Specified x + length exceeds width of image {width}")
+        exit(-1)
+    stop = arguments.x + arguments.length
+    transect = image[arguments.x:stop, arguments.y, band]
 
 plt.plot(transect)
 plt.xlabel('Y Position')
 plt.ylabel('Pixel Value')
-plt.title('{} values of transect through X={}'.format(arguments.band, arguments.position))
-plt.text(30, 62, "Max value = {} at ({},{})".format(np.max(transect), arguments.position, np.argmax(transect)))
+plt.title('{} values of transect through Y={}'.format(arguments.band, arguments.y))
+plt.text(30, 62, "Max value = {} at ({},{})".format(np.max(transect), arguments.y, np.argmax(transect)))
 plt.grid(True)
 plt.show()
 
