678ea6056a76a9a66a855326d85b78ebdae8b6fc
Evan McGinnis <evanmc@yahoo.com>
2025-01-29 22:10:40 +0000

    initial revision

---
+++ util/combine.py
@@ -0,0 +1,51 @@
+
+import argparse
+import os.path
+import sys
+import pandas as pd
+import pandas.errors
+
+parser = argparse.ArgumentParser("Combine CSVs")
+
+parser.add_argument("-i", "--input", action="store", required=True, nargs="*", help="Input CSV")
+parser.add_argument("-o", "--output", action="store", required=True, help="Output CSV")
+parser.add_argument("-f", "--force", action="store_true", required=False, default=False, help="Force overwrite of output file")
+
+arguments = parser.parse_args()
+
+# Refuse to overwrite the file
+if os.path.isfile(arguments.output) and not arguments.force:
+    print(f"Output file exists: {arguments.output}")
+    sys.exit(-1)
+
+data = {}
+for file in arguments.input:
+    # Confirm the input file exists
+    if not os.path.isfile(file):
+        print(f"Unable to access: {file}")
+        sys.exit(-1)
+    try:
+        df = pd.read_csv(file)
+    except pandas.errors.EmptyDataError:
+        print(f"File contains no data: {file}")
+        sys.exit(-1)
+    except pandas.errors.ParserError:
+        print(f"Unable to parse: {file}")
+        sys.exit(-1)
+    data[file] = df
+
+consolidated = None
+for file, frame in data.items():
+    if consolidated is None:
+        consolidated = frame
+    else:
+        consolidated.append(frame)
+
+if consolidated is not None:
+    try:
+        consolidated.to_csv(arguments.output)
+    except FileNotFoundError:
+        print(f"Unable to write combined files to: {arguments.output}")
+        sys.exit(-1)
+
+sys.exit(0)
